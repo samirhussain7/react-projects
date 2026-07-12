3546ecb5e4e79cbe1fd4b08022ce64848778ba70
SAMIR <samirhussain3507@gmail.com>
2026-07-12 19:54:37 +0530

    src added

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import heroImg from "./assets/hero.png";
+import Header from "./components/Header";
+import Hero2 from "./components/Hero2";
+import Hero from "./components/Hero";
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Hero2 />
+    </>
+  );
+}
+
+export default App;
